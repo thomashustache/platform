45022cca7c259b3f056d9399e573cd2bf5b376d5
thomashustache <hustachethomas@gmail.com>
2022-10-02 11:56:42 +0200

    Untrack files in .gitignore

---
+++ agents/A2CAgent.py
@@ -0,0 +1,195 @@
+import torch
+import numpy as np
+
+from agents.agent_utils.memory import MovingAverageMemory
+from .BaseAgent import BaseAgent
+from gym_platform.envs.platform_env import PlatformEnv, ACTION_LOOKUP
+from api.action import deconvert_act, Action
+from .networks.actor_critic import ActorCritic
+from .agent_utils.torch_converter import torch_converter
+
+
+class A2CAgent(BaseAgent):
+    def __init__(self,
+                 lr: float,
+                 init_stds: torch.Tensor,
+                 min_stds: torch.Tensor,
+                 optimize_stds: bool = True,
+                 *args,
+                 **kwargs
+                 ):
+        """Episodic Advantage Actor-Critic
+
+        Args:
+            lr (float): learning rate
+            init_stds (torch.Tensor): Inital values for Stds
+            min_stds (torch.Tensor): _description_
+            optimize_stds (bool, optional): _description_. Defaults to True.
+        """
+        super(A2CAgent, self).__init__(algo_name='A2CAgent', *args, **kwargs)
+
+        # model
+        self.optimize_stds = optimize_stds
+        self.min_stds = min_stds.to(self.device)
+        self.init_stds = init_stds.to(self.device)
+        self.std_decay = 0.999 # useful when Stds are fixed. We manually decrease their values over training.
+        init_mean_action_params = torch.Tensor(self.init_action_params).to(self.device)
+        self.model = ActorCritic(act_size=self.nb_actions,
+                                  logdir=self.logdir,
+                                  hidden_size=128,
+                                  optimize_stds=optimize_stds,
+                                  min_stds=self.min_stds,
+                                  init_means=init_mean_action_params,
+                                  init_stds=self.init_stds.clone()).to(self.device)
+        self.optimizer = torch.optim.Adam(params=self.model.parameters(), lr=lr)
+
+        # counters
+        self.memory = [] # we will fill this with batch_size transitions
+        self.step_counter = 0
+
+        #tensorboard
+        self.actorloss_mem = MovingAverageMemory(100)
+        self.criticloss_mem = MovingAverageMemory(100)
+        self.loss_writer_step = 0
+
+    def learned_act(self, state: np.ndarray) -> np.ndarray:
+        """Select action according to current policy
+
+        Args:
+            state (np.ndarray): state on which to predict the next action
+
+        Returns:
+            np.ndarray: continuous action parameters (The platform env will clamp them if they are out of range)
+        """
+
+        with torch.no_grad():
+            torch_sate = torch_converter(state).to(self.device)
+            dists, _ = self.model(torch_sate)
+            action_params = dists.sample().detach().cpu().data.numpy()
+
+            return action_params
+
+
+    def act(self, state: np.ndarray) -> torch.Tensor:
+        """Sample action from current policy's distribution
+
+        Args:
+            state (np.ndarray): state
+
+        Returns:
+            torch.Tensor: action parameters
+        """
+
+        torch_sate = torch_converter(state).to(self.device)
+        dists, _ = self.model(torch_sate)
+        action_params = dists.sample()
+
+        return action_params
+
+    def train_one_step(self, env: PlatformEnv, action_id_agent: BaseAgent) -> None:
+
+        """Collect a batch of transitions to perform a single parameters update.
+        On-Policy algorithm: The current optimization step is performed based on data collected with the current policy.
+        """
+
+        state = env.get_state()
+        critic_loss = 0
+        actor_loss = 0
+        k = 0
+        reward = 0
+
+        for _ in range(self.batch_size):
+
+            # Choose action id according to fixed policy from Q-Learning agent
+            action_id = action_id_agent.learned_act(state)
+            
+            # Update action choices statistics for display
+            for act_id, a_name in ACTION_LOOKUP.items():
+                if action_id == act_id:
+                    self.action_mem[a_name].add(1)
+                else:
+                    self.action_mem[a_name].add(0)
+
+            # Compute action parameters from Actor's head and value from Critic's head
+            torch_sate = torch_converter(state).to(self.device)
+            dists, value_pred = self.model(torch_sate)
+            action_params = dists.sample()
+
+            # compute log_prob for update rule
+            log_prob = dists.log_prob(action_params)[action_id]
+
+            # Update of the environment
+            params = action_params.clone().detach().cpu().data.numpy()
+            action = deconvert_act(Action(action_id, param=params[action_id]))
+            (next_state, step), reward, done, _, _ = env.step(action)
+            torch_sate = torch_converter(next_state).to(self.device)
+            _, next_value_pred = self.model(torch_sate)
+
+            # Basic 1-step Advantage estimation: A(s, a, theta) = (r + gamma * V(s', theta)) - V(s, theta)
+            # TODO: n-step return estimation, GAE
+            advantage = reward + (1 - done) * self.discount * next_value_pred - value_pred
+            critic_loss += advantage.pow(2)
+            actor_loss -= log_prob * advantage.detach()
+
+            state = next_state
+            self.episode_reward += reward
+            self.episode_step += step
+            k += 1
+            if done:
+                state, _ = env.reset()
+                self.rewardmem.add(self.episode_reward)
+                self.stepmem.add(self.episode_step)
+                k = 0
+                self.episode_reward = 0
+                self.episode_step = 0
+
+        # parameters update
+        self.optimizer.zero_grad()
+        total_loss = (critic_loss + actor_loss) / self.batch_size
+        total_loss.backward()
+        self.optimizer.step()
+
+        # update scalars
+        self.criticloss_mem.add(critic_loss.item() / self.batch_size)
+        self.actorloss_mem.add(actor_loss.item() / self.batch_size)
+
+    def init_training(self):
+        """Training initialization
+        """
+        self._init_training()
+        self.model.train()
+        self.model.init_stds = self.init_stds.clone() # when stsds are not optimized, we reset the stds to their initial values to ensure exploration
+
+    def train(self, env: PlatformEnv, action_id_agent: BaseAgent) -> None:
+        """Main training loop. On-policy update.
+
+        Args:
+            env (PlatformEnv): environment
+            action_id_agent (BaseAgent): Qlearn Agent
+        """
+
+        self.init_training()
+
+        while self.current_optim_step < self.max_optim_steps and not self.check_convergence():
+            self.train_one_step(env=env, action_id_agent=action_id_agent)
+
+            # Main Tensorboard logs
+            self.update_tensorboard_logs()
+
+            # Specific Logs
+            if self.current_optim_step % self.write_frequency == 0:
+                self.loss_writer.add_scalar(self.algo_name + f'/critic_loss', self.criticloss_mem(), self.current_optim_step)
+                self.loss_writer.add_scalar(self.algo_name + f'/actor_loss', self.actorloss_mem(), self.current_optim_step)
+                self.model.update_tensorboard(writer_step=self.writer_step)
+
+            self.current_optim_step += 1
+            
+            # decrease Stds values with decay rate
+            if not self.optimize_stds:
+                self.model.init_stds = torch.amax(torch.stack([self.model.init_stds * self.std_decay, self.min_stds], 0), 0)
+
+
+
+
+
+
---
+++ trainers/QPAMDPTrainer.py
@@ -0,0 +1,170 @@
+from genericpath import exists
+from agents.DQNAgent import DQNAgent
+from agents.A2CAgent import A2CAgent
+from gym_platform.envs.platform_env import PlatformEnv
+from torch.utils.tensorboard import SummaryWriter
+from agents.agent_utils.torch_converter import torch_converter
+from api.action import Action, deconvert_act
+import os
+import glob
+
+from utils.frames_to_vid import load_images_to_video
+from utils.video_to_gif import make_gif_from_video
+from utils.clean_dir import clean_dir
+from utils.py_script_to_txt import save_config_file
+
+
+class QPAMDPTrainer(object):
+
+    def __init__(self,
+                 env: PlatformEnv,
+                 config_script: str,
+                 logdir: str,
+                 experience_name:str,
+                 psearch_agent: A2CAgent,
+                 qlearn_agent: DQNAgent,
+                 k_param: int = 1,
+                 max_qpamdp_steps: int = 10) -> None:
+
+        # agents
+        self.env = env
+        self.psearch_agent = psearch_agent
+        self.qlearn_agent = qlearn_agent
+
+        # optim attributes
+        self.max_steps = max_qpamdp_steps
+        self.k_param = k_param
+
+        # tensorboard attributes
+        self.experience_name = experience_name
+        self.name = 'QPAMDP'
+        self.writer = SummaryWriter(log_dir=logdir + f'/{self.name}')
+        self.tot_writter_steps = 0
+        self.global_optim_step = 0
+        
+        # config
+        self.config_script = config_script
+
+
+        self.set_common_tensorboard_writers()
+
+    def set_common_tensorboard_writers(self) -> None:
+
+        self.qlearn_agent.tb_writer = self.writer
+        self.psearch_agent.tb_writer = self.writer
+
+    def record_video_policy(self, first_call: bool = False) -> None:
+
+        print('generating video...')
+        if not first_call:
+            self.psearch_agent.load('best_model.pth')
+        
+        # pass all models in inference mode.
+        self.psearch_agent.model.eval()
+        self.qlearn_agent.load('best_model.pth')
+        self.qlearn_agent.model.eval()
+        env = PlatformEnv()
+
+        # Make sure all the paths are well created to save the videos, gifs, frames etc
+        video_path = f'results/videos/{self.name}/{self.experience_name}/'
+        gif_folder = f'results/gifs/{self.name}/{self.experience_name}/'
+        frames_path = f'results/frames/{self.name}/{self.experience_name}/optimstep_{self.global_optim_step}/'
+        os.makedirs(frames_path, exist_ok=True)
+        if first_call:
+            os.makedirs(video_path, exist_ok=True)
+            os.makedirs(gif_folder, exist_ok=True)
+            clean_dir(frames_path)
+            clean_dir(video_path)
+            clean_dir(gif_folder)
+            save_config_file(self.config_script, video_path + 'config.txt')
+        
+        
+        # start recording an episode
+        total_reward = 0
+        d = False
+        state, _ = env.reset()
+        while not d:
+
+            action_id = self.qlearn_agent.learned_act(state)
+            params = self.psearch_agent.learned_act(state)
+            action = deconvert_act(Action(action_id, params[action_id]))
+            (state, _), r, d, _, _ = env.step(action)
+            total_reward += r
+
+        total_reward = str(int(total_reward * 100) / 100)
+
+        # Save all frames and create video
+        video_name = video_path + f'optimstep_{self.global_optim_step}_reward-' + total_reward + '.mp4'
+        env.save_render_states(dir=frames_path, prefix='')
+        load_images_to_video(imgs_path=frames_path, video_name=video_name)
+        
+        # rm frames once finished
+        clean_dir(frames_path)
+            
+
+        # Gif from video
+        print('generating gif from video...')
+        gif_name = f'optimstep_{self.global_optim_step}_reward-' + total_reward + '.gif'
+        make_gif_from_video(video_path=video_name,
+                            gif_folder=gif_folder,
+                            gif_name=gif_name,
+                            jpgs_frame_folder='results/jpgs/',
+                            skip_rate=5)
+
+
+    def train(self) -> None:
+        """Reproduce the Q-PAMDP(k) training procedure described in the paper: https://arxiv.org/pdf/1509.01644.pdf
+
+        Inputs:
+
+            Inital weights Theta0 (Action Parameters policy) and W0 (Action policy)
+            Policy Search method (to update Theta)
+            Q-Learn algorithm (to update W)
+
+        Algorithm:
+
+            w <- Q-Learn(oo) with fixed theta
+            Repeat:
+                theta <- P-Search(k) with fixed w
+                w <- Q-Learn(oo) with fixed theta
+            until theta converges
+
+        """
+
+        # 1. w <- Q-Learn(oo) with fixed theta
+        self.psearch_agent.model.eval()
+        self.qlearn_agent.train(env=self.env, action_param_agent=self.psearch_agent)
+
+        # 2. Repeat until theta converges
+        self.record_video_policy(first_call=True)
+        self.global_optim_step += 1
+        while self.global_optim_step < self.max_steps:
+
+            ## update all tb writer steps
+            self.psearch_agent.writer_step = self.qlearn_agent.writer_step
+            print('Training Psearch algorithm...')
+            self.qlearn_agent.load(model_name='best_model.pth')
+            self.qlearn_agent.model.eval()
+            self.psearch_agent.model.train()
+            if self.k_param != -1:
+                for _ in range(self.k_param):
+                    self.psearch_agent.train_one_step(env=self.env, action_id_agent=self.qlearn_agent)
+            else:
+                self.psearch_agent.train(env=self.env, action_id_agent=self.qlearn_agent)
+
+            
+            self.qlearn_agent.writer_step = self.psearch_agent.writer_step
+            print('Training Qlearn algorithm...')
+            self.psearch_agent.load(model_name='best_model.pth')
+            self.psearch_agent.model.eval()
+            self.qlearn_agent.train(env=self.env, action_param_agent=self.psearch_agent)
+
+            # record current policy video
+            self.record_video_policy()
+            self.global_optim_step += 1
+
+
+
+
+
+
