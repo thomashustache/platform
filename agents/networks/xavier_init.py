af43800acd610399b1c84b9922e3784941b9fce0
thomashustache <hustachethomas@gmail.com>
2022-10-02 11:51:11 +0200

    Untrack files in .gitignore

--- agents/networks/xavier_init.py
+++
@@ -1,9 +0,0 @@
-import torch.nn as nn
-
-def xavier_init(module):
-    """Xavier Initialization: https://paperswithcode.com/method/xavier-initialization 
-    """
-    if isinstance(module, nn.Linear):
-        nn.init.xavier_uniform_(module.weight)
-        if module.bias is not None:
-            module.bias.data.zero_()
